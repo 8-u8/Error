59d0ad93b1ea3a7f8c0e6075b5d23f0089cb80a4
kinuITO <itotaka523@gmail.com>
2017-08-03 13:38:49 +0900

    Update ErrorCode.R

--- ErrorCode.R
+++ ErrorCode.R
@@ -12,7 +12,7 @@ rownames(d2)<-c('C','T')
 data <- list(N=nrow(d),X=d$x,Y=d$y,f=d2[d$f,])
 
 #run.
-fit<-stan(file="lastex.stan",data=data, seed = 123)
+fit <- stan(file="lastex.stan",data=data, seed = 123)
 #If seed is 123"4"(over 4 digit number?), error is occured.
 fit2 <- stan(file="lastex.stan",data=data, seed = 1234)
 
